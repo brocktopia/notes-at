043c159664e4a50dbe66456c4939b9e7c77fcb8e
Brock Henderson <brocktopia@gmail.com>
2018-08-23 16:48:23 -0700

    Going to move JS into external file

--- src/components/NoteEdit.vue
+++ src/components/NoteEdit.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
+  <div class="app-container edit">
 
     <header>
       <h2>{{mode === 'edit' ? note.name : 'New Note'}}</h2>
-      <svg class="icon action-icon" @click="closeNote()"><use xlink:href="./dist/symbols.svg#close-note"></use></svg>
-      <svg class="icon action-icon" v-on:click="saveNote()"><use xlink:href="./dist/symbols.svg#save"></use></svg>
+      <span class="button-bar">
+        <svg class="icon action-icon" @click="closeNote()"><use xlink:href="./dist/symbols.svg#close-note"></use></svg>
+        <svg class="icon action-icon" v-on:click="saveNote()"><use xlink:href="./dist/symbols.svg#save"></use></svg>
+      </span>
     </header>
 
-    <div class="content">
+    <div class="content note-edit">
 
-      <label for="noteName">Name</label>
-      <input type="text" id="noteName" v-model="note.name" maxlength="40" placeholder="Name for your note" tabindex="1">
-      <span class="input-info"><span class="char-count">{{note.name.length}}</span> (40 character limit)</span>
+      <div class="name">
+        <label for="noteName">Name</label>
+        <input type="text" id="noteName" v-model="note.name" maxlength="40" placeholder="Name for your note" tabindex="1">
+        <span class="input-info"><span class="char-count">{{note.name.length}}</span> (40 character limit)</span>
+      </div>
 
       <div class="date">{{$moment(note.Created_date).format('LLLL')}}</div>
 
@@ -22,10 +26,10 @@
       </div>
 
       <div class="place">
-        <label for="placeName" v-if="!(note.place && note.place.icon)">
-          <svg class="icon-small"><use xlink:href="./dist/symbols.svg#location"></use></svg>
+        <label v-if="!hasPlace(note)" for="placeName">
+          <svg class="icon-small"><use xlink:href="./dist/symbols.svg#place"></use></svg>
         </label>
-        <label for="placeName" v-if="note.place && note.place.icon">
+        <label v-if="hasPlace(note)" for="placeName">
           <img :src="note.place.icon" width="24" height="24" />
         </label>
         <span :class="note.place && note.place._id ? 'has-place' : 'no-place'" id="placeName">{{note.place && note.place._id ? note.place.name : 'Click the button to add a place'}}</span>
@@ -49,7 +53,9 @@
           :position="{'lat':note.geocode.lat, 'lng':note.geocode.lng}"></gmap-marker>
       </gmap-map>
 
-      <textarea v-model="note.note" placeholder="Your note" rows="32" tabindex="3"></textarea>
+      <div class="note-input">
+        <textarea id="noteNote" v-model="note.note" placeholder="Your note" tabindex="3"></textarea>
+      </div>
 
     </div>
 
@@ -160,6 +166,11 @@
           vm.$emit('close');
         }
       },
+      hasPlace:function(note) {
+        if (note.place && note.place.name) {
+          return true;
+        } else return false;
+      },
       findPlace: function(placeName) {
         //console.log('NoteEditor.findPlace()');
         let options = {
@@ -256,6 +267,14 @@
   }
   .content {
     padding: 20px 20px 8px;
+    display: grid;
+    grid-template-rows: 45px 30px 35px 35px 155px auto;
+  }
+  .note-input {
+    flex: auto;
+  }
+  #noteNote {
+    height: 100%;
   }
   .geocoords, .place {
     height: 30px;
@@ -264,6 +283,9 @@
   }
   .place button {
     vertical-align: top;
+  }
+  .place svg {
+    fill: #ed453b;
   }
   .content > * {
     margin-bottom: 10px;
